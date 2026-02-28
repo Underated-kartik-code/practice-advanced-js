b518bcad08b7f6867cd0ca125441ccff20bf7962
Underated-kartik-code <kartikalokar8@gmail.com>
2026-02-28 15:44:26 +0530

    2nd day

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
-function profile (username,cb){
-    console.log("Fetching profile... ")
-    setTimeout(()=>{
-        cb({id : 123,
-            username : username,
-            Email : "abc@mail.com",
-            age : 20
-        })
-    },2000);
-}
-function postlao (id ,cb){
-    console.log("Fetching posts...")
+function getUser(id,cb){
+    console.log("User fetching....")
     setTimeout(()=>{
         cb({id:id,
-            posts : [1,2,3,4,5,6,7]
+        username:"kartik",
+        bio:"Developer"
+    })
+    },3000)
+}
+function getorder(id,cb){
+    console.log("Your order is coming...")
+    setTimeout(()=>{
+        cb({id:id,
+            order : ['a','b','c','d']
+        })
+    },2000)
+}
+function getmoney(id,cb){
+    setTimeout(()=>{
+        cb({id:id,
+            otp: "abcde"
         })
     },2000)
 }
 
-function savedpost (id ,cb){
-    console.log("fetching savedpost...")
-    setTimeout(()=>{
-        cb({id:id,
-            saved:["hello","hii","good"]
-        })
-    },3000)
-}
- 
-profile("kartik",function(data){
-    console.log(data);
+getUser(123,function(data){
+    console.log(data)
 
-    postlao(data.id,function(posts){
-        console.log(posts)
+    getorder(123,function(order){
+        console.log(order)
 
-        savedpost(data.id,function(saved){
-            console.log(saved)
+        getmoney(123,function(money){
+            console.log("Please verify your otp...")
+            setTimeout(()=>{
+                let input=prompt("give me a captcha");
+
+                if(money.otp==input)console.log("verified please pay")
+                else console.log("its fake")
+            },2000);
         })
-    })   
+    })
 })
